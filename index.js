ac6c277c8b3524ad3eca0425336f88483155eb64
TF997 <ben_townsend@ntlworld.com>
2020-07-25 18:04:57 +0100

    changed env variable

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 require('dotenv').config()
 const app = require('./server')
 
-app.listen(process.env.SERVER_PORT, () => console.log(`Server running on ${process.env.SERVER_PORT}...`))
+app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}...`))
